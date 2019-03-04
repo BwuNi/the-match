29ab5f4e057894cc22fd6deb8951baec6db343c0
bwuni <bwuni@outlook.com>
2019-03-04 17:44:45 +0800

    adjust readme

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# front-end
+# 一个提供 Option 和 Result  的 TypeScript 库
+ts-match
+========
+
+A small matching library for TypeScript. The API is a single function defined as:
+
+```typescript
+function match<T, R>(input: T, defaultCase: IDefaultCase<R>, ...cases: ICase<T, R>[]): R
+````
+
+Example:
+
+```typescript
+import { Some, Ok, Err, None, match } from 'the-match'
+
+const o:Option<String>[]= [Some.new('some'),None.new()]
+const r:Result<string,number>[] = [Err.new(0),Ok.new('Ok')]
+
+
+match(r[0],
+    (ok: string)=>`msg: ${ok}`,
+    (err: number)=>`code: ${err}`
+)
+
+match<string,string>(o[0],
+    (some: string)=>`some: ${some}`,
+    ()=>`none`
+)
+```
--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,19 @@ class Some<T>{
 class Ok<T>{
     v: T
     constructor(v: T) { this.v = v }
-    static new<T>(v: T) { return new Some(v) }
+    static new<T>(v: T) { return new Ok(v) }
 }
 
 class Err<T>{
     v: T
     constructor(v: T) { this.v = v }
-    static new<T>(v: T) { return new Some(v) }
+    static new<T>(v: T) { return new Err(v) }
 }
 
-type None = '__None__'
+class None{
+    constructor() {}
+    static new() { return new None() }
+}
 
 type Option<T> = Some<T> | None
 type Result<T, S> = Ok<T> | Err<S>
@@ -24,11 +27,15 @@ type Result<T, S> = Ok<T> | Err<S>
 function match<R, T, S = void>(
     ...arg: [Option<T>, (some: T) => R, () => R] | [Result<T, S>, (ok: T) => R, (err: S) => R]
 ): R {
-    if (arg[0] === '__None__') return (<() => R>arg[2])()
+    if (arg[0] instanceof None) return (<() => R>arg[2])()
     else if (arg[0] instanceof Some) return arg[1](arg[0].v)
     else if (arg[0] instanceof Ok) return arg[1](arg[0].v)
     else if (arg[0] instanceof Err) return (<((err: S) => R)>arg[2])(arg[0].v)
     else throw 'match type error'
 }
+
+
+
+
 
 export { Some, Ok, Err, None, match }
